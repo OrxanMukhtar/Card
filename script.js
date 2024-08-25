0f04de74aa9ddf22e77def6c8d3cebf25c863bba
OrxanMukhtar <a.muxtarli92@gmail.com>
2024-08-25 09:51:57 +0400

    google analytics

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function copyToClipboard(text) {
         }, 2000);
     });
 }
+
+// Ziyaretçi sayısını al ve artır
+let visitCount = localStorage.getItem('visitCount');
+
+// Ziyaretçi sayısı yoksa, 1'den başla
+if (visitCount === null) {
+    visitCount = 0;
+}
+
+// Ziyaretçi sayısını artır
+visitCount++;
+
+// Ziyaretçi sayısını LocalStorage'da sakla
+localStorage.setItem('visitCount', visitCount);
+
+// Ziyaretçi sayısını ekrana yazdır
+document.getElementById('visitCounter').textContent = `You have visited this page   ${visitCount} times.`;
